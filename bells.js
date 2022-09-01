42922d92b758536b92db95145993d4d0ff5d8810
folatheman <folajunior1@gmail.com>
2022-09-01 11:20:34 -0400

    first commit

---
+++ bells.js
@@ -0,0 +1,23 @@
+var mans=document.getElementById('mans')
+mans.onclick= function()
+{
+    alert('WELCOME TO THE COLLEGE OF OF MANAGEMENT SCIENCE')
+}
+
+var nas=document.getElementById('nas')
+nas.onclick=function()
+{
+    alert('WELCOME TO THE COLLEGE OF NATURAL SCIENCES')
+}
+
+var eng=document.getElementById('eng')
+eng.onclick= function()
+{
+    alert('WELCOME TO THE COLLEGE OF ENGINEERING')
+}
+
+var envs=document.getElementById('envs')
+envs.onclick=function()
+{
+    alert('WELCOME TO THE COLLEGE OF ENVIRONMENTAL SCIENCE')
+}
